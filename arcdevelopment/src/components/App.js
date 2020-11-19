c010c1b9579973a2739644026a08142bd5895ec5
dtruong8 <dtruong8@toromail.csudh.edu>
2020-11-18 22:36:01 -0800

    Completed Styling Inline
    AppBar Logo Next

---
+++ arcdevelopment/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+
+import theme from './ui/Theme';
+import Header from '../components/ui/Header';
+
+
+function App() {
+  return (
+    <ThemeProvider theme={ theme }>
+      <Header />
+      Hello World!
+    </ThemeProvider>
+  );
+}
+
+export default App;
