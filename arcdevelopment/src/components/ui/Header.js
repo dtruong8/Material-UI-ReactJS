26de2a5812272b678f6ec4d735dfe63a151cd2a1
dtruong8 <dtruong8@toromail.csudh.edu>
2020-11-20 14:20:57 -0800

    Set Up Navigation Bar with Tabs
    Next is section 3: adding header features - menus and responsive design

--- arcdevelopment/src/components/ui/Header.js
+++ arcdevelopment/src/components/ui/Header.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, useScrollTrigger } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { AppBar, Toolbar, useScrollTrigger, Tab, Tabs, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -17,19 +20,86 @@ function ElevationScroll(props) {
 
 const useStyles = makeStyles(theme => ({
     toolbarMargin: {
-        ...theme.mixins.toolbar
+        ...theme.mixins.toolbar,
+        marginBottom: "3em"
+    },
+    logo: {
+        height: "5em"
+    },
+    logoContainer:{
+        padding: 0,
+        "&:hover": {
+            backgroundColor: "transparent"
+        }
+    },
+    tabContainer: {
+        marginLeft: 'auto'
+    },
+    tab: {
+        ...theme.typography.tab,
+        minWidth: 10,
+        marginLeft: "25px"
+    },
+    button:{
+        ...theme.typography.estimate,
+        borderRadius: "50px",
+        marginLeft: "50px",
+        marginRight: "25px",
+        height: "45px"
     }
 }));
 
 export default function Header(prop) {
     const classes = useStyles()
+    const [value, setValue] = useState(0);
+    const handleChange = (e, value) => { 
+        setValue(value)
+    };
+
+    useEffect (() => {
+        if (window.location.pathname === "/" && value !== 0){
+            setValue(0);
+        }
+        else if (window.location.pathname === "/services" && value !== 1){
+            setValue(1);
+        }
+        else if (window.location.pathname === "/revolution" && value !== 2){
+            setValue(2);
+        }
+        else if (window.location.pathname === "/about" && value !== 3){
+            setValue(3);
+        }
+        else if (window.location.pathname === "/contact" && value !== 4){
+            setValue(4);
+        }
+    }, [value]);
 
     return (
         <React.Fragment>
             <ElevationScroll>
                 <AppBar position="fixed">
-                    <Toolbar>
-                        <Typography variant="h3"> Arc Development </Typography>
+                    <Toolbar disableGutters>
+                        <Button 
+                            component={Link} 
+                            to="/" 
+                            onClick={() => setValue(0)} 
+                            disableRipple
+                            className={classes.logoContainer}>
+                            <img alt="company logo" className={classes.logo} src={logo} />
+                        </Button>
+                        <Tabs value={value} 
+                              onChange={handleChange}
+                              className={classes.tabContainer}
+                              indicatorColor="primary">
+                            <Tab className={classes.tab} component={Link} to="/" label="Home" />
+                            <Tab className={classes.tab} component={Link} to="/services" label="Services" />
+                            <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution" />
+                            <Tab className={classes.tab} component={Link} to="/about" label="About Us" />
+                            <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us" />
+                        </Tabs>
+                        <Button variant="contained" color="secondary" className={classes.button}>
+                            Free Estimate
+                        </Button>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
