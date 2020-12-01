7bc923d82418dac600ece249f071ade9a14ea3e5
dtruong8 <dtruong8@toromail.csudh.edu>
2020-11-30 21:14:52 -0800

    Update Footer.js

--- arcdevelopment/src/components/ui/Footer.js
+++ arcdevelopment/src/components/ui/Footer.js
@@ -4,6 +4,9 @@ import { Grid, Hidden } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 import footerAdornment from '../../assets/Footer Adornment.svg';
+import facebook from '../../assets/facebook.svg';
+import twitter from '../../assets/twitter.svg';
+import instagram from '../../assets/instagram.svg';
 
 const useStyles = makeStyles(theme => ({
     footer: {
@@ -34,6 +37,23 @@ const useStyles = makeStyles(theme => ({
     },
     gridItem: {
         margin: "3em"
+    },
+    icon: {
+        height: "4em",
+        width: "4em",
+        [theme.breakpoints.down("xs")]: {
+            height: "2.5em",
+            width: "2.5em"
+        }
+        
+    },
+    socialContainer:{
+        position: "absolute",
+        marginTop:"-6em",
+        right: "1.5em",
+        [theme.breakpoints.down("xs")]: {
+            right: "0.6em"
+        }
     }
 }));
 
@@ -41,7 +61,7 @@ export default function Footer(props) {
     const classes = useStyles();
 
     return (
-            <footer className={classes.footer}>
+        <footer className={classes.footer}>
             <Hidden mdDown>
                 <Grid container justify="center" className={classes.mainContainer}>
                     <Grid item className={classes.gridItem}>
@@ -104,12 +124,23 @@ export default function Footer(props) {
                         </Grid>
                     </Grid>
                 </Grid>
+                </Hidden>
                 <img
                     alt="black decoractive slash"
                     src={footerAdornment}
                     className={classes.adornment}
                 />
-                </Hidden>
-            </footer>
+                <Grid container justify="flex-end" className={classes.socialContainer} spacing={2}>
+                    <Grid item component={"a"} href="https://www.facebook.com" rel="noopener noreferrer" target="_blank">
+                        <img alt="facebook logo" src={ facebook } className={classes.icon} />
+                    </Grid>
+                    <Grid item component={"a"} href="https://www.twitter.com" rel="noopener noreferrer" target="_blank">
+                        <img alt="twitter logo" src={ twitter } className={classes.icon}/>
+                    </Grid>
+                    <Grid item component={"a"} href="https://www.instagram.com" rel="noopener noreferrer" target="_blank">
+                        <img alt="instagram logo" src={ instagram } className={classes.icon}/>
+                    </Grid>
+                </Grid>
+        </footer>
     );
 }
